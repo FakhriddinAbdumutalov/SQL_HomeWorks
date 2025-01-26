3ca5d6f1562c9357c0c52456a4cae2ed5ba22dfd
Fakhriddin Abdumutalov <104620739+FakhriddinAbdumutalov@users.noreply.github.com>
2025-01-26 05:54:48 -0500

    the homework3 remodified

--- HomeWork_3.sql
+++ HomeWork_3.sql
@@ -1,93 +1,123 @@
 Create Database Policy
+go
 
 use Policy
-
-Create table iam_policies(
-PolicyID int primary key,
-PolicyName varchar(50), PolicyType varchar(50))
 go
 
-create table iam_roles(
-RoleID int primary key,
-RoleName varchar(50),
-RoleType varchar(50),
-RoleActions varchar(50))
-go
+create table iam_roles (
+    db_id int primary key,
+    id int not null,
+    name varchar(50) not null,
+    actions varchar(255),
+    type varchar(50)
+);
 
-create table iam_projects_graveyard(
-ProjectGraveyardID int primary key)
-go
+create table iam_policies (
+    db_id int primary key,
+    id int not null,
+    name varchar(50) not null,
+    type varchar(50)
+);
 
-create table iam_projects(
-ProjectID int primary key,
-ProjectName varchar(50),
-ProjectType varchar(50))
-go
+create table iam_statements (
+    db_id int primary key,
+    effect varchar(50) not null,
+    actions varchar(255),
+    resources varchar(255),
+    policy_id int,
+    role_id int,
+    foreign key (policy_id) references iam_policies(db_id),
+    foreign key (role_id) references iam_roles(db_id)
+);
 
-create table iam_members(
-MemberID int primary key,
-MembersName varchar(50))
-go
+create table iam_members (
+    db_id int primary key,
+    name varchar(50) not null
+);
 
-create table iam_policy_members(
-MemberID int foreign key references iam_members(MemberID))
-go
+create table iam_policy_members (
+    member_id int,
+    policy_id int,
+    primary key (member_id, policy_id),
+    foreign key (member_id) references iam_members(db_id),
+    foreign key (policy_id) references iam_policies(db_id)
+);
 
-create table iam_policy_projects(
-PolicyID int foreign key references iam_policies(PolicyID),
-ProjectID int foreign key references iam_projects(ProjectID))
-go
+create table iam_projects (
+    db_id int primary key,
+    id int not null,
+    name varchar(50) not null,
+    type varchar(50)
+);
 
-create table iam_statements(
-StatementID int primary key,
-Effect text,
-actions varchar(30), 
-resources varchar(50),
-PolicyID int foreign key references iam_policies(PolicyID)
-go
+create table iam_policy_projects (
+    policy_id int,
+    project_id int,
+    primary key (policy_id, project_id),
+    foreign key (policy_id) references iam_policies(db_id),
+    foreign key (project_id) references iam_projects(db_id)
+);
 
+create table iam_statement_projects (
+    project_id int,
+    statement_id int,
+    primary key (project_id, statement_id),
+    foreign key (project_id) references iam_projects(db_id),
+    foreign key (statement_id) references iam_statements(db_id)
+);
 
-create table iam_statement_projects(
-ProjectID int foreign key references iam_projects(ProjectID),
-StatementID int foreign key references iam_statements(StatementID))
-go
+create table iam_role_projects (
+    role_id int,
+    project_id int,
+    primary key (role_id, project_id),
+    foreign key (role_id) references iam_roles(db_id),
+    foreign key (project_id) references iam_projects(db_id)
+);
 
-create table iam_role_projects(
-RoleID int foreign key references iam_roles(RoleID), 
-ProjectID int foreign key references iam_projects(ProjectID))
-go
+create table iam_staged_project_rules (
+    db_id int primary key,
+    name varchar(50) not null,
+    type varchar(50),
+    deleted bit not null,
+    project_id int,
+    foreign key (project_id) references iam_projects(db_id)
+);
 
-create table iam_staged_project_rules(
-ProjectStagedRuleID int primary key, 
-ProjectStagedRuleName varchar(50), 
-ProjectStagedRuleType varchar(50), 
-check (ProjectStagedRuleType is not null),
-ProjectID int foreign key references iam_projects(ProjectID))
-go
+create table iam_project_rules (
+    db_id int primary key,
+    name varchar(50) not null,
+    type varchar(50),
+    project_id int,
+    foreign key (project_id) references iam_projects(db_id)
+);
 
-create table iam_staged_rule_conditions(
-StagedRuleConditionID int primary key,
-ProjectStagedRulesID int foreign key references iam_staged_project_rules(ProjectStagedRuleID),
-value int, 
-StagedRuleConditionAttribute varchar(50), 
-StagedRuleConditionOperator varchar(50),
-ProjectStagedRuleID int foreign key references iam_staged_project_rules(ProjectStagedRuleID))
-go
+create table iam_projects_graveyard (
+    db_id int primary key,
+    id int not null,
+    name varchar(50) not null,
+    type varchar(50),
+    project_id int,
+    foreign key (project_id) references iam_projects(db_id)
+);
 
-create table iam_project_rules(
-ProjectRuleID int primary key, 
-ProjectRuleName varchar(50), 
-ProjectRuleType varchar(50),
-ProjectID int foreign key references iam_projects(ProjectID))
-go
+create table iam_staged_rule_conditions (
+    db_id int primary key,
+    rule_db_id int not null,
+    value varchar(255),
+    attribute varchar(50),
+    operator varchar(50),
+    foreign key (rule_db_id) references iam_staged_project_rules(db_id)
+);
 
-create table iam_rule_conditions(
-RuleConditionID int primary key, 
-value int, 
-RuleConditionAttribute varchar(50),
-RuleConditionOperator varchar (50),
-ProjectRuleID int foreign key references iam_project_rules(ProjectRuleID))
-go
+create table iam_rule_conditions (
+    db_id int primary key,
+    rule_db_id int not null,
+    value varchar(255),
+    attribute varchar(50),
+    operator varchar(50),
+    foreign key (rule_db_id) references iam_project_rules(db_id)
+);
+
 
 --Hometask
 --Whenever a user enters blank ('', '  ') or null to F(address) it should automatically change it to Tashkent
@@ -98,21 +128,21 @@ insert into G values ('Romano', 'Italy')
 insert into G values ('Federico', 'Bilbao')
 insert into G values ('Sergio', null)
 
-CREATE TRIGGER trg_AddressDefault
-ON G
-INSTEAD OF INSERT, UPDATE
-AS
-BEGIN
+create trigger trg_AddressDefault
+on G
+instead of insert, update
+as
+begin
     -- Insert valid data, replacing NULL or blank addresses with 'Tashkent'
-    INSERT INTO G (name, address)
-    SELECT 
+    insert into G (name, address)
+    select 
         name,
-        CASE 
-            WHEN address IS NULL OR LTRIM(RTRIM(address)) = '' THEN 'Tashkent'
-            ELSE address
-        END
-    FROM inserted;
-END;
-GO
+        case 
+            when address IS NULL OR ltrim(rtrim(address)) = '' then 'Tashkent'
+            else address
+        end
+    from inserted;
+end;
+go
 
 
